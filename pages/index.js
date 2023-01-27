3758a9b3e8951ff4a6f0f12c6077a20d70072b96
Matias Benites <matiasbenites665@gmail.com>
2023-01-27 19:08:35 -0300

    feat:Added text's comp to banner

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,18 @@ import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 const Banner = ({ purpose }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    {purpose}
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+      <Text fontSize="3x1" fontWeight="bold">
+        {purpose}
+      </Text>
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+    </Box>
   </Flex>
 );
 
