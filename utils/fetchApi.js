0d925d8a8a2624f9fc2c1c09c19b8223e2f5e903
Matias Benites <matiasbenites665@gmail.com>
2023-01-28 20:59:26 -0300

    fix:public api_key changed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "c6be4098d7msh6ef4d9160c2f4eep178710jsn2ff64045a826",
+      "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
