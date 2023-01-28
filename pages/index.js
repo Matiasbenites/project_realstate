ef578e9f4c0facd4a5e2be2835bc90b29432c691
Matias Benites <matiasbenites665@gmail.com>
2023-01-28 11:29:26 -0300

    feat:Props added to banner comp

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import { title } from "process";
 
-const Banner = ({ purpose }) => (
+const Banner = ({
+  purpose,
+  title,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner" />
     <Box p="5">
       <Text color="gray.500" fontSize="sm" fontWeight="medium">
         {purpose}
       </Text>
-      <Text fontSize="3x1" fontWeight="bold">
-        {purpose}
+      <Text fontSize="3xl" fontWeight="bold">
+        {title}
+        <br />
+        {title2}
       </Text>
-      <Text color="gray.500" fontSize="sm" fontWeight="medium">
-        {purpose}
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">
+        {desc1}
+        <br />
+        {desc2}
       </Text>
+      <Button fontSize="xl" bg="blue.300" color="white">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
     </Box>
   </Flex>
 );
