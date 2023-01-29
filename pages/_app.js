8d33ad930cd30feed7b53a9246a327ce1ce9970f
Matias Benites <matiasbenites665@gmail.com>
2023-01-29 14:30:46 -0300

    feat:Render app modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import { AppProps } from "next/app";
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps } = AppProps) {
-  return <Component {...pageProps} />;
+import Layout from "../components/Layout";
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head></Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+      ;
+    </>
+  );
 }
+
+export default App;
